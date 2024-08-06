64a6eb0dee695a1388acc7d665cd9ad191003b82
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-08-06 17:13:09 +0800

    First commit

---
+++ HashMap.mjs
@@ -0,0 +1,46 @@
+class HashMap {
+    constructor() {
+        this.bucket = new Array(16);
+    }
+
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+
+        return hashCode;
+    }
+
+    set(key, value) {
+        const hashCode = this.hash(key) % 16;
+
+        this.bucket[hashCode] = value;
+    }
+
+    print() {
+        for (let i = 0; i < this.bucket.length; i++) {
+            console.log(i + " " + this.bucket[i]);
+        }
+    }
+}
+
+const test = new HashMap();
+
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
+
+// console.log(test.bucket);
+test.print();
